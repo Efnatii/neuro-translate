74be246f1caf461a5dfdb9d1789ed67ea0578a23
Efnatii <efnatii@yandex.ru>
2026-01-18 17:35:39 +0300

    Revert "Reset translation state and reload page when cancelling translation"

--- extension/popup.js
+++ extension/popup.js
@@ -358,9 +358,7 @@ async function sendCancel() {
   }
   updateTranslationVisibility(false);
   updateTranslationVisibilityStorage(false);
-  await clearTranslationStatus(tab.id);
   setTemporaryStatus('Перевод для этой страницы отменён.');
-  await chrome.tabs.reload(tab.id);
 }
 
 async function sendTranslateRequest() {
@@ -539,16 +537,6 @@ async function updateTranslationVisibilityStorage(visible) {
   const { translationVisibilityByTab = {} } = await chrome.storage.local.get({ translationVisibilityByTab: {} });
   translationVisibilityByTab[activeTabId] = visible;
   await chrome.storage.local.set({ translationVisibilityByTab });
-}
-
-async function clearTranslationStatus(tabId) {
-  const { translationStatusByTab = {} } = await chrome.storage.local.get({ translationStatusByTab: {} });
-  delete translationStatusByTab[tabId];
-  await chrome.storage.local.set({ translationStatusByTab });
-  if (activeTabId === tabId) {
-    currentTranslationStatus = null;
-    renderStatus();
-  }
 }
 
 async function handleOpenDebug() {
